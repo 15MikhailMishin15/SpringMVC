b2da507af1f0391b0672885343a0c9f0eefdeb0d
Mikhail Mishin <mishin_mihail_98@mail.ru>
2024-07-12 22:32:51 +0300

    commit1

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,39 @@
+package web.model;
+
+public class Car {
+
+    private String model;
+    private String manufacturer;
+    private int year;
+
+    public Car(String model, String manufacturer, int year) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.year = year;
+    }
+
+    // Getters and setters
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
